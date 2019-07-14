60596477e66e2171e918fd77b2162e8dc65b4dc9
Will Wilson <qtvhao@gmail.com>
2019-07-14 12:30:43 +0700

    add method Actions.toString()

--- app/Helpers/Wilson/Form/Collection/Actions.php
+++ app/Helpers/Wilson/Form/Collection/Actions.php
@@ -6,5 +6,11 @@ namespace App\Helpers\Wilson\Form\Collection;
 
 class Actions extends BaseCollection
 {
+    public function __toString()
+    {
+        return implode('', array_map(function($field) {
+            return (string)$field;
+        }, $this->items));
+    }
 
 }
