186523e0243826b90ce7da4cf6b9a7f9c09668ca
Will Wilson <qtvhao@gmail.com>
2019-07-14 12:30:25 +0700

    add method Fields.toString()

--- app/Helpers/Wilson/Form/Collection/Fields.php
+++ app/Helpers/Wilson/Form/Collection/Fields.php
@@ -2,7 +2,16 @@
 
 namespace App\Helpers\Wilson\Form\Collection;
 
+use App\Helpers\Wilson\Form\Field\BaseField;
+
 class Fields extends BaseCollection
 {
+    public function __toString()
+    {
+        return implode('', array_map(function($field) {
+            /** @var BaseField|Fields $field */
+            return (string)$field->__toString();
+        }, $this->items));
+    }
 
 }
