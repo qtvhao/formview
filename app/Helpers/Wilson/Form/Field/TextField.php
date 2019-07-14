1d5267b02927bdffccd2ffaad30cde4d36433a13
Will Wilson <qtvhao@gmail.com>
2019-07-14 12:29:57 +0700

    add method toString for TextField

--- app/Helpers/Wilson/Form/Field/TextField.php
+++ app/Helpers/Wilson/Form/Field/TextField.php
@@ -7,4 +7,16 @@ namespace App\Helpers\Wilson\Form\Field;
 class TextField extends BaseField
 {
 
+    public function __toString()
+    {
+        $id = 'ws-field-' . $this->getId();
+        return <<<HTML
+<div class="form-group">
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="$id" placeholder="">
+    </div>
+  </div>
+HTML;
+    }
+
 }
