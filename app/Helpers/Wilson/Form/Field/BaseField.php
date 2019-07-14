37547772310cb85bff23317a38bc74650f92b70d
Will Wilson <qtvhao@gmail.com>
2019-07-14 12:28:35 +0700

    adding id

--- app/Helpers/Wilson/Form/Field/BaseField.php
+++ app/Helpers/Wilson/Form/Field/BaseField.php
@@ -6,8 +6,34 @@ namespace App\Helpers\Wilson\Form\Field;
 
 class BaseField
 {
+    protected $id;
+
+    public function __toString()
+    {
+        return "No __toString implemented for this field";
+    }
+
     public static function text()
     {
         return new TextField();
     }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return BaseField
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
 }
